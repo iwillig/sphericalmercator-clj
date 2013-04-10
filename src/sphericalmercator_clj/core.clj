3ce96bb65f1b49b64bd79ce6519bb80ae86da757
Ivan Willig <iwillig@gmail.com>
2013-04-09 23:41:28 -0400

    Added more links explaining the sources of the port

--- src/sphericalmercator_clj/core.clj
+++ src/sphericalmercator_clj/core.clj
@@ -1,11 +1,13 @@
 (ns sphericalmercator-clj.core
   (:import [java.lang Math]))
 
-;; this is a port of dev seeds sphericalmercator.js
-;; https://github.com/mapbox/node-sphericalmercator
-
-;; its also take from openstreetmap's
+;; take from openstreetmap's
 ;; http://svn.openstreetmap.org/applications/rendering/mapnik/generate_tiles.py
+;; also ported from MapBox's sphericalmercator.js
+;; https://github.com/mapbox/node-sphericalmercator;
+;; based on the TMS spec
+;; http://wiki.osgeo.org/wiki/Tile_Map_Service_Specification
+
 
 (def cache     (atom {}))
 (def EPSLN     1.0e-10)
@@ -22,32 +24,36 @@
 
 (defrecord SphericalMercator [size Bc Cc Zc Ac]
   Convertable
+
   (lonlat->pixel [self ll zoom]
-    (let [d    ((:Zc self) zoom)
+    (let [d       ((:Zc self) zoom)
           acZoom  ((:Ac self) zoom)
-          f    (Math/min (Math/max (Math/sin (* D2R (ll 1)))  -0.9999) 0.9999)
-          x    (Math/round (float (+ d (* (ll 0)  ((:Bc self) zoom)))))
-          y    (Math/round
-                (+ d (* (* 0.5 (Math/log (/ (+ 1 f)
-                                            (- 1 f))))
-                        (- ((:Cc self) zoom)))))]
+          f       (Math/min (Math/max (Math/sin (* D2R (ll 1)))  -0.9999) 0.9999)
+          x       (Math/round (float (+ d (* (ll 0)  ((:Bc self) zoom)))))
+          y       (Math/round
+                   (+ d (* (* 0.5 (Math/log (/ (+ 1 f)
+                                               (- 1 f))))
+                           (- ((:Cc self) zoom)))))]
       [(if (> x acZoom) acZoom x)
        (if (> y acZoom) acZoom y)]))
   
   (pixel->ll [self px zoom]
-    (let [zcZoom ((:Zc self) zoom)
-          g  (/ (- (px 1) zcZoom)  (- ((:Cc self) zoom)))
-          lon (/ (- (px 0) zcZoom) (- ((:Bc self) zoom)))
-          lat (* R2D (-  (* 2 (Math/atan (Math/exp g))) (* 0.5 Math/PI)))]
+    (let [zcZoom     ((:Zc self) zoom)
+          g          (/ (- (px 1) zcZoom)  (- ((:Cc self) zoom)))
+          lon        (/ (- (px 0) zcZoom) (- ((:Bc self) zoom)))
+          lat        (* R2D (-  (* 2 (Math/atan (Math/exp g))) (* 0.5 Math/PI)))]
       [lon lat]))
 
+  ;; returns a vector with the following values
+  ;; [left bottom right top]
   (xyz->bbox      [self x y zoom]
     (let [size         (:size self)
+          y            (- (- (Math/pow 2 zoom) 1) y)
           lower-left   [(* x size)  (* (+ y 1) size)]
           upper-right  [(* (+ x 1) size) (* y size)]]
 
-      [(pixel->ll self lower-left zoom)
-       (pixel->ll self upper-right zoom)])))
+      (concat (pixel->ll self lower-left zoom)
+              (pixel->ll self upper-right zoom)))))
 
 (defn build-tileset [size]
   (let [steps (take 30 (iterate (partial * 2) size))]
--- test/sphericalmercator_clj/core_test.clj
+++ test/sphericalmercator_clj/core_test.clj
@@ -2,19 +2,32 @@
   (:use clojure.test
         sphericalmercator-clj.core))
 
+;; 15/9646/12322
+;; 15/5570/13076
+
 (def sm (spherical-mercator 256))
 
 (deftest test-pixel->longlat
+
   (testing ""
     (let [ll (pixel->ll sm [1 2] 1)]
       (is (= [179.296875 84.92832092949963] ll)))))
 
 (deftest text-longlat->pixel
+
   (testing ""
     (let [pixel (lonlat->pixel sm [1 2] 1)]
       (is (= [257 253] pixel)))))
 
 (deftest test-bbox
+
   (testing ""
     (let [bbox (xyz->bbox sm 0 0 0)]
-      (is (= [[180.0 -85.05112877980659] [-180.0 85.0511287798066]] bbox)))))
+      (is (= [180.0 -85.05112877980659 -180.0 85.0511287798066] bbox))))
+
+  (testing "bbox conversation"
+    (let [bbox (xyz->bbox sm 0 0 1)
+          bk   (xyz->bbox sm 603 770 11)
+          poco (xyz->bbox sm 4825 6162 14)]
+      (println bk)
+      (is (= [-180 -85.05112877980659 0 0] bbox)))))
